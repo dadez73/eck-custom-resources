fa7f4397f31949c25a6323e10fdfa0965ac5071d
Davide Doninelli <davide.doninelli@eoc.ch>
2025-08-29 10:53:50 +0200

    api test

--- cmd/main.go
+++ cmd/main.go
@@ -81,7 +81,7 @@ func main() {
 	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "The address the metrics endpoint binds to. "+
 		"Use :8443 for HTTPS or :8080 for HTTP, or leave as 0 to disable the metrics service.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.IntVar(&syncPeriod, "sync-period", 1, "The period between reconciles.")
+	flag.IntVar(&syncPeriod, "sync-period", 10, "The period between reconciles.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
@@ -197,7 +197,7 @@ func main() {
 			config.GetCertificate = metricsCertWatcher.GetCertificate
 		})
 	}
-	d := time.Duration(syncPeriod) * time.Minute
+	d := time.Duration(syncPeriod) * time.Hour
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
 		Metrics:                metricsServerOptions,
